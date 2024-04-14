4c6da25fd46fca0a17e5428f93b33b69bc4be696
alexlenkov-ptn <alex.lenkov123@outlook.com>
2024-04-14 22:41:51 +0300

    add the logic for MainActivity

--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.cafe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView editTextName;
+    private TextView editTextPassword;
+    private Button buttonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Intent intent = new Intent(this, OrderDetailActivity.class);
+        Log.d("MainActivity", "onCreate");
+        initViews();
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = editTextName.getText().toString().trim();
+                String passWord = editTextPassword.getText().toString().trim();
+
+                if (userName.isEmpty() || passWord.isEmpty()) {
+                    Toast.makeText(
+                            MainActivity.this,
+                            R.string.error_fields_empty,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }else {
+                    launchNextScreen(userName);
+                }
+            }
+        });
+
+
+
+
+    }
+
+    public void launchNextScreen(String userName) {
+        Intent intent = makeOrderActivity.newIntent(this, userName);
         startActivity(intent);
+    }
+
+    private void initViews() {
+        editTextName = findViewById(R.id.editTextName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSignIn = findViewById(R.id.buttonSignIn);
     }
 }
